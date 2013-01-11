92497e0a26a0d12861f95eedb45ecb44e4db7ee7
Michael Marino <mmarino@gmail.com>
2013-01-11 11:29:43 +0100

    Adding ability to change the length of the output from SignalGeneration

--- Reader/SignalGeneration.cc
+++ Reader/SignalGeneration.cc
@@ -3,7 +3,7 @@
 
 ClassImp(SignalGeneration)
 
-SignalGeneration::SignalGeneration(const std::string& filename) 
+SignalGeneration::SignalGeneration(const std::string& filename) : fWFMinLength(0), fWFDelayLength(0) 
 {
   fWPot.SetFile(filename);
 }
@@ -12,13 +12,23 @@ EXODoubleWaveform SignalGeneration::GetSignalWithDriftPath(const DriftTrajectory
 {
   const DriftTrajectory::PathType& path = dt.GetLastDriftedPath();
   EXODoubleWaveform retWF;
-  retWF.SetLength(path.size());
+  retWF.SetLength(fWFDelayLength+path.size());
   retWF.SetSamplingPeriod(dt.GetSamplingPeriod());
+  if (retWF.size() == 0) return retWF;
 
   for (size_t i=0;i<path.size();i++) {
     if (!fWPot.ReadValues(path[i].X(), path[i].Y(), path[i].Z())) break;
-    retWF[i] = fWPot.fValues[0];
+    retWF[fWFDelayLength + i] = fWPot.fValues[0];
   }
  
+  size_t oldLength = retWF.size();
+  if (retWF[oldLength-1] > 0.999) retWF[oldLength-1] = 1.;
+
+  if (retWF.size() < fWFMinLength) {
+    double lastVal = retWF[oldLength - 1];
+    retWF.SetLength(fWFMinLength);
+    for (size_t i=oldLength;i<fWFMinLength;i++) retWF[i] = lastVal; 
+  } 
+  
   return retWF; 
 }
--- Reader/SignalGeneration.h
+++ Reader/SignalGeneration.h
@@ -15,8 +15,13 @@ class SignalGeneration {
     
     EXODoubleWaveform GetSignalWithDriftPath(const DriftTrajectory& dt) const;
 
+    void SetTotalWaveformMinLength(size_t i) { fWFMinLength = i; }
+    void SetWaveformDelayTerm(size_t i) { fWFDelayLength = i; }
+
   protected:
     mutable ThreeDWeightPotentialReader fWPot;
+    size_t fWFMinLength;
+    size_t fWFDelayLength;
 
   ClassDef(SignalGeneration, 0)
   
