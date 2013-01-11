f1a1a0110b0dc1026fbbb0624dbbb79579e7a95a
Michael Marino <mmarino@gmail.com>
2013-01-11 11:29:15 +0100

    Using names of typedefs in LinkDef.h

--- Reader/LinkDef.h
+++ Reader/LinkDef.h
@@ -1,5 +1,5 @@
-#pragma link C++ class ThreeDFieldReader<1>+;
-#pragma link C++ class ThreeDFieldReader<3>+;
+#pragma link C++ class ThreeDWeightPotentialReader+;
+#pragma link C++ class ThreeDElectricFieldReader+;
 
 #pragma link C++ defined_in "DriftTrajectory.h";
 #pragma link C++ defined_in "SignalGeneration.h";
